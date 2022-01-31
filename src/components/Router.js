bc73568ce1a4030355d294a5257fdffe2c588c95
박재현 <eric7213@naver.com>
2022-01-31 11:41:22 +0900

    2.2

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import Auth from "../routes/Auth"
-import Home from "../routes/Home"
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "routes/Auth"
+import Home from "routes/Home"
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const AppRouter = ({isLoggedIn}) => {
   return (
     <Router>
-      <Routes>
+      <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
@@ -19,7 +18,7 @@ const AppRouter = () => {
             <Auth />
           </Route>
         )}
-      </Routes>
+      </Switch>
     </Router>
   );
 };
